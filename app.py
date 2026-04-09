a33affe8d806aee4a65e57957fb8eeec4c832cc5
Patrick <biyagapatrick@gmail.com>
2026-04-10 01:39:53 +0200

    Docker ci cd yml file ready for dockerhub

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app=Flask(__name__)
 
+@app.route("/hello")
+def hello():
+    return "Hello, Welcome to Devops-Team"
+
+@app.route("/about_us")
+def about_us():
+    return "About Devops-Team"
+
 @app.route("/")
-def home():
-    return "Hello World!"
+def index():
+    return "Homepage of Devops-Team"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000)
