48071a6dee7b42fe8d25dbcbb28cc75c0a16abcd
Patrick <biyagapatrick@gmail.com>
2026-04-10 01:45:12 +0200

    Docker ci cd yml file ready for dockerhub

--- test_app.py
+++ test_app.py
@@ -1,7 +1,19 @@
 from app import app
 
-def test_home():
+def test_index():
     response=app.test_client().get("/")
 
     assert response.status_code==200
-    assert response.data== b"Hello World!"
+    assert response.data== b"Homepage of Devops-Team"
+
+def test_hello():
+    response=app.test_client().get("/hello")
+
+    assert response.status_code==200
+    assert response.data== b"Hello, Welcome to Devops-Team"
+
+def about_us():
+    response=app.test_client().get("/about_us")
+
+    assert response.status_code==200
+    assert response.data== b"About Devops-Team"
